98dc67fbbffaf0f3cea116efc02aee54ba2e2fd9
vpkarthi50 <vpkarthi50@gmail.com>
2022-12-27 20:50:09 +0530

    java-variables

---
+++ type_casting.java
@@ -0,0 +1,19 @@
+package daily_task;
+
+public class type_casting {
+    public static void main(String[] args) {
+       //wideing method int to double
+        int a=20;
+        double b=a;
+        System.out.println(a);
+        System.out.println(b);
+        //narrowing method double to float
+        float c=50.52f;
+        double d=(double) c;
+        System.out.println(c);
+        System.out.println(d);
+        //Using char to find  the ASCII value
+        char e=90;
+        System.out.println(e); 
+    }
+}
---
+++ variable.java
@@ -0,0 +1,11 @@
+package daily_task;
+
+public class variable {
+   public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        System.out.println(a);
+        System.out.println(b);
+   }
+}
